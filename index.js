63cc6afa892c1dc5528b09e21253cdeb40fc7871
Tuhin35 <ariaan59@gmail.com>
2023-07-20 17:08:23 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,307 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express');
+const cors = require('cors')
+require('dotenv').config();
+var jwt = require('jsonwebtoken');
+const stripe = require("stripe")(process.env.STRIPE_SECRET)
+const app = express();
+const port =process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json())
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uidhp96.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+function VerifyJwt(req,res,next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+      res.status(401).send({
+          message:'unauthorized access'
+      })
+  }
+  const token = authHeader.split(' ')[1];
+      jwt.verify(token,process.env.ACCESS_TOKEN,function(err,decoded){
+        if (err) {
+          return res.status(403).send({message: 'unauthorized access'});
+          
+        }
+        req.decoded = decoded;
+        next();
+      })
+  }
+
+
+async function run() {
+  try {
+   
+    await client.connect();
+    console.log("database connect");
+
+  } catch {
+    // Ensures that the client will close when you finish/error
+   console.log(console.error())
+  }
+}
+run()
+
+app.get('/',(req,res)=>{
+    res.send('travel guru server is running')
+});
+
+const productCollection = client.db('Online_resell').collection('Product')
+ const categoryCollection = client.db('Online_resell').collection('categories')
+ const orderCollection =  client.db('Online_resell').collection('orders')
+ const usersCollections = client.db('Online_resell').collection('users')
+ const paymentsCollection = client.db('Online_resell').collection('payments')
+
+ app.post('/jwt',(req,res)=>{
+  const user = req.body;
+  const token = jwt.sign(user,process.env.ACCESS_TOKEN)
+  res.send({token})
+  
+})
+app.get('/jwt', async(req,res)=>{
+const email =req.query.email;
+const query = {email:email}
+const user = await usersCollections.findOne(query)
+if (user) {
+const token = jwt.sign({email},process.env.ACCESS_TOKEN)
+return res.send({accessToken:token});
+}
+res.status(403).send({accessToken: ''})
+})
+
+
+
+   app.get('/categories',async (req,res)=>{
+    const query = {};
+    const category = await categoryCollection.find(query).toArray();
+    res.send(category);
+
+   })
+ 
+  
+   app.get ("/products/category/:id",async(req,res)=>{
+    const id = req.params.id ;
+    const getSingleId = await productCollection?.filter((p)=>p.c_id == id);
+    if (!getSingleId) {
+    res.send("Valu kore khujjo");          
+    }
+    else{
+        res.send(getSingleId)
+    }
+
+})
+
+
+   app.get('/products', async(req,res)=>{
+    const page =parseInt( req.query.page);
+    const size = parseInt(req.query.size);
+    console.log(page,size)
+const query = {};
+
+const cursor = productCollection.find(query)
+const products = await cursor.skip(page*size).limit(size).toArray();
+const count = await productCollection.estimatedDocumentCount()
+res.send({count,products})
+})
+
+app.get('/product/:id',async(req,res)=>{
+
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)};
+  // console.log(id)
+  const product = await productCollection.findOne(query)
+  res.send(product)
+
+})
+
+
+    //orders Api 
+        // post Kora hosse data create korar jonno
+
+        app.post('/orders', async(req,res)=>{
+          const order = req.body;
+          const result = await orderCollection.insertOne(order)
+          res.send(result)
+         })
+
+         app.get('/orders', async(req,res)=>{
+          // const page =parseInt( req.query.page);
+          // const size = parseInt(req.query.size);
+        
+   const query = {};
+
+   const cursor = orderCollection.find(query)
+   const orders =await cursor.toArray();
+  //  const count = await orderCollection.estimatedDocumentCount()
+res.send(orders)
+
+      })
+
+      app.get('/order',VerifyJwt, async(req,res)=>{
+        //   const page =parseInt( req.query.page);
+       //    const size = parseInt(req.query.size);
+          // console.log(page,size)
+         
+          const decoded = req.decoded;
+         //  console.log("inside orders api", decoded);
+
+          if (decoded.email !==req.query.email) {
+
+           res.status(403).send({
+               message:"Unauthorized access"
+           })
+          }
+
+    let query = {};
+
+    if (req.query.email) {
+       query={
+           email:req.query.email
+       }
+       
+    }
+
+
+    const cursor = orderCollection.find(query)
+    const orders = await cursor.toArray();
+  //  const count = await orderCollection.estimatedDocumentCount()
+res.send(orders)
+       })
+
+       app.get('/orders/:id',async(req,res)=>{
+           const id = req.params.id;
+           const query = {_id : new ObjectId(id)}
+           const order = await orderCollection.findOne(query);
+           res.send(order)
+         })
+
+       app.patch('/orders/:id',async (req,res)=>{
+           const id = req.params.id;
+           const status = req.body.status;
+           const query = {_id: new ObjectId(id) }
+           const updatedDoc = {
+               $set:{
+                   status:status
+               }
+           }
+           const result = await orderCollection.updateOne(query,updatedDoc);
+           res.send(result)
+       })
+
+       app.delete('/orders/:id', async(req,res)=>{
+const id = req.params.id;
+const query = {_id: new ObjectId(id)}
+const result = await orderCollection.deleteOne(query);
+res.send(result)
+
+       })
+
+//payments
+app.post('/create-payment-intent',async(req,res)=>{
+  const booking = req.body;
+  const price = booking.price;
+  const amount = price* 100;
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    currency : 'usd',
+    amount : amount,
+    "payment_method_types":[
+      "card"
+    ]
+  })
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  })
+
+})
+
+// save payment data
+app.post('/payments',async(req,res)=>{
+const payment = req.body;
+const result = await paymentsCollection.insertOne(payment);
+const id = payment.bookingId
+const filter = {_id: new ObjectId(id)}
+const updatedDoc = {
+$set:{
+paid: true,
+transactionId:payment.transactionId
+}
+}
+
+const updatedResult = await orderCollection.updateOne(filter,updatedDoc)
+res.send(result)
+})
+// user admin server
+  //user 
+  app.get('/users', async (req, res) => {
+    const query = {};
+    const users = await usersCollections.find(query).toArray();
+    res.send(users)
+
+
+  })
+
+  // save user data
+  app.post('/users', async (req, res) => {
+    const user = req.body;
+    const result = await usersCollections.insertOne(user);
+    res.send(result)
+  })
+
+  app.delete('/users/:id', async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await usersCollections.deleteOne(query);
+    // res.send(result)
+
+  })
+
+
+
+      // check admin
+app.get('/users/admin/:email',async(req,res)=>{
+
+const email= req.params.email;
+const query = {email}
+const user = await usersCollections.findOne(query);
+res.send({isAdmin: user?.role ==='admin'})
+})
+
+
+// make admin
+app.put('/users/admin/:id', async (req, res) => {
+
+const id = req.params.id;
+
+const filter = { _id: new ObjectId(id) }
+const options = { upsert: true };
+const updatedDoc = {
+  $set: {
+    role: 'admin'
+  }
+}
+const result = await usersCollections.updateOne(filter, updatedDoc, options);
+res.send(result)
+})
+
+
+
+app.listen(port, ()=>{
+    console.log(`online-resell-server is running on port,${port}`)
+})
